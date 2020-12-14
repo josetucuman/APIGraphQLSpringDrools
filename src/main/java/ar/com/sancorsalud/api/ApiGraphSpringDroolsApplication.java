43e1f7b874600f0ad99c2fa30e1e8f0b697609ca
Jose Gomez <josegelimergomez@gmail.com>
2020-12-14 16:22:19 -0300

    Primer Commit GraphQL Con Springboot

---
+++ src/main/java/ar/com/sancorsalud/api/ApiGraphSpringDroolsApplication.java
@@ -0,0 +1,13 @@
+package ar.com.sancorsalud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGraphSpringDroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGraphSpringDroolsApplication.class, args);
+	}
+
+}
